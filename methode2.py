02255c4d46a983078c5cd070e9398cc87f6ee60e
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-20 11:20:44 +0100

    Update methode2.py

--- methode2.py
+++ methode2.py
@@ -2,9 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def greedy_algorithm(data):
+def get_donnees(data,yi,xi):
   """
   data : dictionnaire des instances
+  yi : liste des indices de critères
+  xi : liste des indices des objets
+  renvoie une liste des donnees du probleme a considerer
+  """
+  res = []
+  for v in yi : 
+    for x in 
+  
+def init_glouton(data,yi,xi):
+  """
+  data : dictionnaire des instances
+  yi : liste des indices de critères
+  xi : liste des indices des objets
   """
   sol=[]
+  
+  
   
