635bc5ba75fda52d6a428f18c59ad053ac191990
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-20 18:25:01 +0100

    debut de rbls

--- methode2.py
+++ methode2.py
@@ -3,12 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tools as *
 
+def pmr(fonc, yx, xprim):
+    """
+    func : fonction d agregation
+    """
+    return m
 
-recherce_locale(pb):
+def mr():
+    return
+def mmr():
+    return
+
+rbls(pb, eps):
     """
     pb : dict des donnees du probleme considere
+    implementation du regret-based local search
     renvoie la solution du sac a dos
     """
     sol = init_glouton(pb)
+    
+    
     
     
