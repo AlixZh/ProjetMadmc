cbf88145ff1c129c41059678d3e83c9d7264f9a3
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-20 18:54:53 +0100

    Update methode2.py

--- methode2.py
+++ methode2.py
@@ -14,14 +14,18 @@ def mr():
 def mmr():
     return
 
-rbls(pb, eps):
+rbls(pb,eps,max_it):
     """
     pb : dict des donnees du probleme considere
     implementation du regret-based local search
     renvoie la solution du sac a dos
     """
     sol = init_glouton(pb)
-    
+    it = 0
+    ameliore = True
+    while(ameliore and it < max_it) : 
+        xvoisin = voisinage(pb, sol) : 
+            
     
     
     
