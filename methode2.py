a319180684060cb896bb5de15a00bc5a5b09cd5e
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-20 11:40:49 +0100

    Update methode2.py

--- methode2.py
+++ methode2.py
@@ -2,17 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_donnees_pb(data,vp,xi):
-  """
-  data : dictionnaire des instances
-  vp : liste des indices de critères
-  xi : liste des indices des objets
-  renvoie une liste des donnees du probleme a considerer
-  """
-  res = []
-  for x in xi :
-    res.append([data["i"][x][p] for p in vp])
-  return res
+
   
 def init_glouton(data,vp,xi):
   """
