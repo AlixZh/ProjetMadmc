58b0ae92897ff0a0ff50199e69639ca8e9a19ca9
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-20 11:57:32 +0100

    Update methode2.py

--- methode2.py
+++ methode2.py
@@ -5,13 +5,18 @@ import tools as *
 
   
 def init_glouton(data,vp,xi):
-  """
-  data : dictionnaire des instances
-  vp : liste des indices de critères
-  xi : liste des indices des objets
-  """
-  sol=[]
-  pb = get_donnees_pb(data,vp,xi)
-  
+    """
+    data : dictionnaire des instances
+    vp : liste des indices de critères
+    xi : liste des indices des objets
+    """
+    sol=[]
+    pb = get_donnees_pb(data,vp,xi)
+    W = wmax(data,vp,xi)
+    somme = np.sum(pb[:,1:],1)
+    for i in range(len(xi)):
+        
+    while(np.sum(sol) < W):
+    
   
   
