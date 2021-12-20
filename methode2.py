2a44d1772c87099cb4188f3755c0377df2b847b6
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-21 00:21:07 +0100

    ajout de la fonction echange11 mod

--- methode2.py
+++ methode2.py
@@ -14,6 +14,30 @@ def mr():
 def mmr():
     return
 
+def echange_11(pb,x_init,obj_retire):
+    """
+    pb : une liste des donnees du probleme
+    x_init : une solution trouvé solution 
+    obj_rerire : 
+    renvoie la voisinage de cette solution
+    """
+    list_x_change=[]
+    nb_obj=pb["n"]
+    w_=pb["wi"]#liste poids
+    v_=pb["v"]#liste profit
+    W=pb["W"]
+    L=x_init.copy()
+    L.remove(obj_retire)
+    poids_=np.sum([w_[i] for i in x_init],axis=0)-w_[obj_retire]#poids sans obj_retire
+    for obj in range(nb_obj) :
+        if(obj!=obj_retire):
+            if(w_[obj]+poids_<=W):
+                #ne peut qu'ajouter cet obj
+                if(any(v_[obj]>v_[obj_retire])):
+                    #ameliorer le profit
+                    list_x_change.append([obj]+L)
+    return list_x_change
+
 def voisin(pb,courante):
     """
     pb : donnees du probleme
