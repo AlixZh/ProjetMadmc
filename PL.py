f5a514b342a6df2b86e45d0afc87598747c7573c
AlixZh <72941464+AlixZh@users.noreply.github.com>
2022-01-13 17:56:03 +0100

    correction

--- PL.py
+++ PL.py
@@ -1,84 +1,50 @@
 from gurobipy import *
 import numpy as np
-import tools as *
-
-
-def opt_pl(pb,fonc,xi,nbcont = 1):
-	"""
-	pb : dict du pb considere
-	fonc : fonction d agregation
-	x : liste des indices des objets a prendre
-	nb_cont : nombre de contrainte
-	"""
-	nbvar=pb["n"] #nombre d objet possible
-	lignes = range(nbcont)
-	colonnes = range(nbvar)
-
-
-	#matrice des contraintes
-	a = np.array(pb["wi"])
-	# Second membre
-	b = pb["W"]
-
-	# Coefficients de la fonction objectif
-	c = y(pb,xi)
-
-	m = Model("SD")     
-		    
-	# declaration variables de decision
-	w = []
-	for i in colonnes:
-		w.append(m.addVar(vtype=GRB.INTEGER, lb=0, ub=1, name="w%d" % (i+1)))
-
-	# maj du modele pour integrer les nouvelles variables
-	m.update()
-
-	obj = LinExpr();
-	obj =0
-	for j in colonnes:
-		obj += c[j] * x[j]
-		    
-	# definition de l'objectif
-	m.setObjective(obj,GRB.MAXIMIZE)
-
-	# Definition des contraintes
-	for i in lignes:
-		m.addConstr(quicksum(a[i][j]*x[j] for j in colonnes) <= b[i], "Contrainte%d" % i)
-
-	# Resolution
-	m.optimize()
-
-
-	print("")                
-	print('Solution optimale:')
-	for j in colonnes:
-		print('x%d'%(j+1), '=', w[j].w)
-	print("")
-	print('Valeur de la fonction objectif :', m.objVal)
 
 
 
+def opt_pl(pb,fonc,w):
+    """
+    pb : dict du pb considere
+    fonc : fonction d agregation
+    w : liste des poids
+    """
+    nbvar=pb["n"] #nombre d objet possible
+    colonnes = range(nbvar)
 
 
+    #matrice des contraintes
+    a = pb["wi"]
+    # Second membre
+    b = pb["W"]
 
 
+    m = Model("SD")     
+
+    # declaration variables de decision
+    x = []
+    for i in colonnes:
+        x.append(m.addVar(vtype=GRB.INTEGER, lb=0, ub=1, name="x%d" % (i+1)))
+
+    # maj du modele pour integrer les nouvelles variables
+    m.update()
+
+    obj = LinExpr();
+    obj = fonc(pb,w,x,False)
+    print("obj ",obj)
+    # definition de l'objectif
+    m.setObjective(obj,GRB.MAXIMIZE)
+    print(m)
+    # Definition des contraintes
+    m.addConstr(quicksum(a[j]*x[j] for j in colonnes) <= b, "Contrainte%d" % 1)
+
+    # Resolution
+    m.optimize()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print("")                
+    print('Solution optimale:')
+    for j in colonnes:
+        print('x%d'%(j+1), '=', x[j].x)
+    print("")
+    print('Valeur de la fonction objectif :', m.objVal)
