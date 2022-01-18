2e552117d9b928bea0e481187e6d68836aaeebd0
zitongSophie <62668648+zitongSophie@users.noreply.github.com>
2022-01-18 17:23:59 +0100

    pl pour solution opt de owa

--- PL.py
+++ PL.py
@@ -1,6 +1,6 @@
 from gurobipy import *
 import numpy as np
-
+import tools as ts
 
 
 def opt_pl(pb,fonc,w):
@@ -18,23 +18,25 @@ def opt_pl(pb,fonc,w):
     # Second membre
     b = pb["W"]
 
-
-    m = Model("SD")     
+    env = Env(empty=True)
+    env.setParam("OutputFlag",0)
+    env.start()
+    m = Model("SD",env=env)     
 
     # declaration variables de decision
     x = []
     for i in colonnes:
-        x.append(m.addVar(vtype=GRB.INTEGER, lb=0, ub=1, name="x%d" % (i+1)))
+        x.append(m.addVar(vtype=GRB.INTEGER, lb=0, ub=1, name="x%d" % (i)))
 
     # maj du modele pour integrer les nouvelles variables
     m.update()
 
     obj = LinExpr();
     obj = fonc(pb,w,x,False)
-    print("obj ",obj)
+    #print("obj ",obj)
     # definition de l'objectif
     m.setObjective(obj,GRB.MAXIMIZE)
-    print(m)
+    #print(m)
     # Definition des contraintes
     m.addConstr(quicksum(a[j]*x[j] for j in colonnes) <= b, "Contrainte%d" % 1)
 
@@ -48,3 +50,69 @@ def opt_pl(pb,fonc,w):
         print('x%d'%(j+1), '=', x[j].x)
     print("")
     print('Valeur de la fonction objectif :', m.objVal)
+
+
+
+def opt_pl_owa(pb,w):
+    """
+    pb : dict du pb considere
+    w : liste des poids
+    """
+
+    env = Env(empty=True)
+    env.setParam("OutputFlag",0)
+    env.start()
+    m = Model("OWA",env=env)     
+    w.sort()
+    #w=w[::-1]
+    # declaration variables de decision
+    r=[]
+    x = []
+    b=[]
+    N=range(pb["n"])
+    P=range(pb["p"])
+    for i in N:
+        x.append(m.addVar(vtype=GRB.BINARY, lb=0,ub=1, name="x%d" % (i)))
+    for i in P:
+        r.append(m.addVar(vtype=GRB.CONTINUOUS,name="r%d" % (i)))
+    for i in P:
+        bi=[]
+        for j in P:
+            bi.append(m.addVar(vtype=GRB.BINARY, lb=0, ub=1, name="b%d" % (i*10+j)))
+        b.append(bi)
+    # maj du modele pour integrer les nouvelles variables
+
+    m.update()
+
+    obj = LinExpr();
+    obj =quicksum(w[j]*r[j] for j in P)
+    #print("obj ",obj)
+    # definition de l'objectif
+    m.setObjective(obj,GRB.MAXIMIZE)
+    #print(m)
+    # Definition des contraintes
+
+    m.addConstr(quicksum(pb["wi"][j]*x[j] for j in N) <= pb["W"])
+    y=LinExpr();
+    y=ts.y_sol(pb,x,list_ind = False)
+    #grand nombre M M is a sufficiently large constant (larger than any possible difference between various indi- vidual outcomes yi)
+    M=10000
+    for i in P:
+        for k in P:
+            m.addConstr(r[k]<=y[i]+M*b[i][k])
+    for k in P:
+        m.addConstr(quicksum(b[i][k] for i in P)==k)
+
+    #m.addConstr(quicksum(a[j]*x[j] for j in colonnes) <= b, "Contrainte%d" % 1)
+
+    # Resolution
+    m.optimize()
+
+
+    print("")                
+    print('Solution optimale:')
+    for j in N:
+        print('x%d'%(j+1), '=', x[j].x)
+    print("")
+    print('Valeur de la fonction objectif :', m.objVal)    
+
