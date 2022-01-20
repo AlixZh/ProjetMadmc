6582f902bf4ab30c697df0e3b26329f3b3356592
AlixZh <72941464+AlixZh@users.noreply.github.com>
2022-01-20 18:06:56 +0100

    correction

--- test_comparaison.py
+++ test_comparaison.py
@@ -52,16 +52,14 @@ def execution_comparaison(eps = 0.1, nb_test = 50,nb_critere = [2,3,4,5,6],nb_ob
                 temp.append(test_m2.une_experience_sp())
                 #x2,temps2,nb_question2,y_ag2 = test_m2.une_experience_sp()
                 
-
+                print(temp)
                 
                 #Stockage SP
                 l_temps_sp[p,n,0,i] = temp[0][1]
-                
                 for _type in range(2):
-                    l_temps_sp[p,n,_type + 1,i] = temp[_type][1]
-                    l_nb_Q_sp[p,n,_type,i] = temp[_type][2]
-                    l_cpt_erreur_sp[p,n,_type,i],l_erreur_relative_sp[p,n,_type,i] = erreur(pb,temp[_type][0],temp[0][0],temp[_type][-1],temp[0][-1])
-                     
+                    l_temps_sp[p,n,_type + 1,i] = temp[_type+1][1]
+                    l_nb_Q_sp[p,n,_type,i] = temp[_type+1][2]
+                    l_cpt_erreur_sp[p,n,_type,i],l_erreur_relative_sp[p,n,_type,i] = erreur(pb,temp[_type+1][0],temp[0][0],temp[_type+1][-1],temp[0][-1])
                 
                 #OWA
                 temp = []
@@ -72,6 +70,8 @@ def execution_comparaison(eps = 0.1, nb_test = 50,nb_critere = [2,3,4,5,6],nb_ob
                 #Stockage OWA
                 l_temps_owa[p,n,0,i] = temp[0][1]
                 for _type in range(2):
-                    l_temps_owa[p,n,_type,i] = temp[_type][1]
-                    l_nb_Q_owa[p,n,_type,i] = temp[_type][2]
-                    l_cpt_erreur_owa[p,n,_type,i] ,l_erreur_relative_owa[p,n,_type,i] = erreur(pb,temp[_type][0],temp[0][0],temp[_type][-1],temp[0][-1])
+                    l_temps_owa[p,n,_type+1,i] = temp[_type+1][1]
+                    l_nb_Q_owa[p,n,_type,i] = temp[_type+1][2]
+                    l_cpt_erreur_owa[p,n,_type,i] ,l_erreur_relative_owa[p,n,_type,i] = erreur(pb,temp[_type+1][0],temp[0][0],temp[_type+1][-1],temp[0][-1])
+                    
+    return l_temps_sp,l_nb_Q_sp, l_erreur_relative_sp,l_cpt_erreur_sp,l_temps_owa ,l_nb_Q_owa,l_erreur_relative_owa ,l_cpt_erreur_owa 
